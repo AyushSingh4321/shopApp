7b6eae6f4033c214af4ee57faf57ea24d9d61638
AyushSingh4321 <ayushksb11@gmail.com>
2024-01-30 00:22:00 +0530

    FavouritesUsingProviders

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,17 +1,24 @@
-class Product {
+import 'package:flutter/material.dart';
+
+class Product with ChangeNotifier {
   final String id;
   final String title;
   final String description;
   final String imageUrl;
   final double price;
   bool isFavourite;
-  
+
   Product({
     required this.id,
     required this.title,
     required this.description,
     required this.price,
     required this.imageUrl,
-    this.isFavourite =false,
+    this.isFavourite = false,
   });
+
+  void toggleFavouriteStatus() {
+    isFavourite = !isFavourite;
+    notifyListeners();
+  }
 }
