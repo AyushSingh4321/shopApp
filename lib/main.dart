ef48b9e3891f3ebe439590743a9d69075c65ef47
AyushSingh4321 <ayushksb11@gmail.com>
2024-01-29 23:04:08 +0530

    providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './providers/products.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
           primaryColor: Colors.purple,
           colorScheme:
               ThemeData().colorScheme.copyWith(secondary: Colors.deepOrange),
@@ -17,11 +22,12 @@ class MyApp extends StatelessWidget {
             foregroundColor: Colors.white,
           ),
           fontFamily: 'Lato',
-          ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx)=>ProductDetailScreen(),
-      },
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
     );
   }
 }
