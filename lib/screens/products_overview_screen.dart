128996aae176efd786d083e468d98c3eee923935
AyushSingh4321 <ayushksb11@gmail.com>
2024-02-08 23:50:19 +0530

    fetchingDataFromServer

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -5,6 +5,7 @@ import '../widgets/products_grid.dart';
 import '../widgets/badge.dart' as MyBadge;
 import 'package:provider/provider.dart';
 import '../providers/cart.dart';
+import '../providers/products.dart';
 
 enum FilterOptions {
   Favourites,
@@ -18,6 +19,36 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavourites = false;
+  var _isInit = true;
+  var _isLoading = false;
+  @override
+  void initState() {
+    // Provider.of<Products>(context).fetchAndSetProducts();// context WON'T WORK here, can work  if listen is set to false
+    //A HACK!
+    // Future.delayed(Duration.zero).then((_) {
+    //   Provider.of<Products>(context).fetchAndSetProducts();
+    // });
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+         _isLoading = true;
+      });
+     
+      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+        setState(() {
+           _isLoading = false;
+        });
+       
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,7 +92,10 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showOnlyFavourites),
+      body: _isLoading? Center(
+              child: CircularProgressIndicator(),
+            ) 
+            : ProductsGrid(_showOnlyFavourites),
     );
   }
 }
