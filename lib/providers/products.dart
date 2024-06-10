98afb9a9349171a4328e76fdc3efc8f6850fb372
AyushSingh4321 <ayushksb11@gmail.com>
2024-06-10 21:09:44 +0530

    addingTokenToOngoingHTTPRequestsUsingProxyProvider

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -52,6 +52,9 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
+  final String? authToken;
+  Products(this.authToken,this._items);
+
   List<Product> get items {
     // if (_showFavouritesOnly) {
     //   return _items.where((prodItem) => prodItem.isFavourite).toList();
@@ -69,7 +72,7 @@ class Products with ChangeNotifier {
 
   Future<void> fetchAndSetProducts() async {
     Uri url = Uri.parse(
-        'https://flutter-update-7d5bf-default-rtdb.firebaseio.com/products.json');
+        'https://flutter-update-7d5bf-default-rtdb.firebaseio.com/products.json?auth=$authToken');
     try {
       final response = await http.get(url);
       // print(json.decode(response.body));
