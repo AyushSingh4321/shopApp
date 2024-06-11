92f6d33dc2cd9bb7d962289ce789d7d932e35350
AyushSingh4321 <ayushksb11@gmail.com>
2024-06-12 01:35:23 +0530

    addingAnimationToAuthCardUsingAnimatedBuilder

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -118,7 +118,7 @@ class _AuthCardState extends State<AuthCard>
     ).animate(
       CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn),
     );
-    _heightAnimation.addListener(() => setState(() {}));
+    // _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -215,14 +215,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 340 : 260,
-        height: _heightAnimation.value.height,
-        constraints:
-            // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 340 : 260),
-            BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+            // height: _authMode == AuthMode.Signup ? 340 : 260,
+            height: _heightAnimation.value.height,
+            constraints:
+                // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 340 : 260),
+                BoxConstraints(minHeight: _heightAnimation.value.height),
+            width: deviceSize.width * 0.75,
+            padding: EdgeInsets.all(16.0),
+            child: ch),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
